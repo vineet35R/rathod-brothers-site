c0083ffd21868f28e37e1c090e2d68984a18af1d
Vineet Rathod <161231768+vineet35R@users.noreply.github.com>
2026-01-02 18:03:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { Pool } = require('pg');
+if (process.env.NODE_ENV!=="production"){
 require('dotenv').config(); // Load environment variables from .env file
+}
 const app = express();
 const port = process.env.PORT || 3000;
 
